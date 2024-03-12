3c47e28c87f8d750cff0ed853d1f40e989a10f0e
Vinicius Saes <viniciusaesouza@gmail.com>
2024-03-12 20:10:19 -0300

    Aula 2.2 e 2.6

--- Java/PrimeiraClasse/src/Main.java
+++ Java/PrimeiraClasse/src/Main.java
@@ -2,9 +2,26 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Esse é o Screen Match");
         System.out.println("Filme: Top Gun: Maverick");
+
         int anoDeLancamento = 2022;
+
         System.out.println("Ano de Lançamento: " + anoDeLancamento);
+
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+        double media = (9.8 + 6.3 + 6.5 + notaDoFilme) / 4;
+        System.out.println(media);
+        String sinopse = """
+                Filme de aventura
+                Com galã dos anos 80
+                Ruim e pra velho
+                Ano de lançamento:
+                """+ anoDeLancamento;
+        //Comentário de linha única.
+        System.out.println(sinopse);
+        /*Comentário
+          De mais de uma linha
+         */
+
     }
 }
