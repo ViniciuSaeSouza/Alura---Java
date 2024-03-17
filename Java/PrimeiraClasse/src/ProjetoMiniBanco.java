686cd7d4a18451f4512a64e2670da0c0a57172b0
Vinicius Saes <viniciusaesouza@gmail.com>
2024-03-17 18:09:45 -0300

    refactor

--- Java/PrimeiraClasse/src/ProjetoMiniBanco.java
+++ Java/PrimeiraClasse/src/ProjetoMiniBanco.java
@@ -13,52 +13,47 @@ public class ProjetoMiniBanco {
         double saldoInicial = input.nextDouble();
         double saldoAtual = saldoInicial;
         //System.out.println(" ");
-        System.out.println("****************************************************************");
+        System.out.println("\n****************************************************************");
         System.out.println("Dados iniciais do Cliente ");
         System.out.println("Nome:                          " + nomeCliente);
         System.out.println("Tipo Conta:                    " + tipoDeConta);
         System.out.println("Saldo Inicial:                 " + "R$" + saldoAtual);
-        System.out.println("****************************************************************");
+        System.out.println("****************************************************************\n");
 
 
         int operacaoEscolhida = 0;
         while (operacaoEscolhida != 4) {
-            System.out.println("Operações");
-            System.out.println(" ");
+            System.out.println("Operações\n");
             System.out.println("""
                     1- Consultar Saldo
                     2- Receber Valor
                     3- Transferir Valor
-                    4- Encerrar
-                    """);
+                    4- Encerrar""");
 
 
             System.out.println("Digite a operação escolhida: ");
             operacaoEscolhida = input.nextInt();
             switch (operacaoEscolhida) {
                 case 1:
-                    System.out.println("O saldo atual é: R$" + saldoAtual);
-                    System.out.println(" ");
+                    System.out.println("O saldo atual é: R$" + saldoAtual+"\n");
+
                     break;
                 case 2:
                     System.out.println("Digite o valor a ser recebido: ");
                     double valorRecebido = input.nextDouble();
                     saldoAtual += valorRecebido;
-                    System.out.println("O saldo atualizado é: R$" + saldoAtual);
-                    System.out.println(" ");
+                    System.out.println("O saldo atualizado é: R$" + saldoAtual+"\n");
                     break;
                 case 3:
                     System.out.println("Digite o valor a ser transferido: ");
                     double valorTransferido = input.nextDouble();
                     if (valorTransferido > saldoAtual){
                         System.out.println("Não há saldo suficiente para esta operação: ");
-                        System.out.println("Seu saldo atual é: R$" + saldoAtual);
-                        System.out.println(" ");
+                        System.out.println("Seu saldo atual é: R$" + saldoAtual+"\n");
                         break;
                     } else {
                         saldoAtual -= valorTransferido;
-                        System.out.println("O saldo atualizado é: R$" + saldoAtual);
-                        System.out.println(" ");
+                        System.out.println("O saldo atualizado é: R$" + saldoAtual+"\n");
                         break;
                     }
                 case 4:
