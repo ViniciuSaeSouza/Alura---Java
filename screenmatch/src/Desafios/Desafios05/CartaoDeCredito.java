cfb1f61d0d63c0d464d76a07e4e5c35642365b8f
Vinicius Saes <viniciusaesouza@gmail.com>
2024-07-22 14:36:56 -0300

    Update CartaoDeCredito.java

--- screenmatch/src/Desafios/Desafios05/CartaoDeCredito.java
+++ screenmatch/src/Desafios/Desafios05/CartaoDeCredito.java
@@ -25,4 +25,14 @@ public class CartaoDeCredito {
     public List<Compra> getCompras() {
         return compras;
     }
+
+    public boolean lancaCompra(Compra compra){
+        if (this.saldo > compra.getValor()){
+            this.saldo -= compra.getValor();
+            this.compras.add(compra);
+            return true;
+        }
+
+        return false;
+    }
 }
