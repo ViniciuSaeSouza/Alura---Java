c456c5c3d79aee275ba3ac4056798765139a5671
Vinicius Saes <viniciusaesouza@gmail.com>
2024-08-18 20:32:47 -0300

    Update screenmatch

---
+++ screenmatch/src/Desafios/Desafios06/LivroGoogle.java
@@ -0,0 +1,4 @@
+package Desafios.Desafios06;
+
+public record LivroGoogle(String title, String authors, String publishedDate) {
+}
--- screenmatch/src/Desafios/Desafios06/Main.java
+++ screenmatch/src/Desafios/Desafios06/Main.java
@@ -3,19 +3,28 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import javax.swing.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        String json = """
-                {
-                    "nome" : "Saes",
-                    "idade" : 28,
-                    "cidade" : "São Paulo"
-                }
-                """;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Título: ");
+        var titulo = sc.next();
+        String endereco = "https://www.googleapis.com/books/v1/volumes?q="+ titulo + "&key=AIzaSyCWVp2yMwsxJ23DPcrLovFOruFwMtGAYEI";
 
-
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
+        System.out.println(json);
         Gson gson = new Gson();
-        Pessoa pessoa = gson.fromJson(json, Pessoa.class);
-        System.out.println(pessoa);
+        LivroGoogle livroGoogle = gson.fromJson(json, LivroGoogle.class);
+        System.out.println(livroGoogle.title());
     }
 }
