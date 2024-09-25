2800e81885b6c2920631546a20d9aa6c00b083a9
Vinicius Saes <viniciusaesouza@gmail.com>
2024-09-24 21:58:33 -0300

    Hikari connection pool added

--- bytebank/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ bytebank/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,5 +1,8 @@
 package br.com.alura.bytebank;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,9 +11,19 @@ public class ConnectionFactory {
 
     public Connection recuperaConexao() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=Vinimysqlkobe08$");
+            return createDataSource().getConnection();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
+    public HikariDataSource createDataSource(){
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
+        config.setUsername("root");
+        config.setPassword("Vinimysqlkobe08$");
+        config.setMaximumPoolSize(1);
+        return new HikariDataSource(config);
+    }
+
 }
